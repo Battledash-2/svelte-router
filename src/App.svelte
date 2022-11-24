adbc05edcc08e8f926ce7400c4b9b9f75b048b21
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-11-24 18:10:29 -0500

    l

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,9 @@
 <script>
 	import Route from "./Route.svelte";
 	import Router from "./Router.svelte";
-	import Thing from "./test/Thing.svelte";
-	import Thong from "./test/Thong.svelte";
 </script>
 <Router>
-	<Route path="/hello" component={Thing} exact />
-	<Route path="/yellow" component={Thong} exact />
-
-	<Route path=".*">
-		<h1>No not found L</h1>
+	<Route path="/hello" exact>
+		<h1>Hello!</h1>
 	</Route>
 </Router>
