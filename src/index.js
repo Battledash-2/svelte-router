2cbc28655af1042ef9f6781da698c50a731364e1
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-11-24 22:14:15 -0500

    Cleanup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Router } from './Router.svelte';
+export { default as Route } from './Route.svelte';
+export { default as Link } from './Link.svelte';
+export { navigate } from './helpers/navigation.js';
