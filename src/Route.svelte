0108ce55a29c303ab6eb3730fde58b08135adcb7
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2023-08-18 16:10:01 -0400

    better :)

--- src/Route.svelte
+++ src/Route.svelte
@@ -2,12 +2,7 @@
 	import { getContext } from 'svelte';
 	import { matched, ROUTER } from './helpers/contexts';
 	import { pathJoin } from './helpers/navigation';
-	import {
-		getRegexp,
-		parseRoute,
-		routeMatch,
-		routeMatchText,
-	} from './helpers/parseRoute';
+	import { getRegexp, parseRoute, routeMatch, routeMatchText } from './helpers/parseRoute';
 
 	export let path = '';
 	export let component = null;
@@ -16,7 +11,7 @@
 	export let index = false;
 
 	const { registerRoute, home } = getContext(ROUTER);
-	path = pathJoin([home, path]);
+	// path = pathJoin([home, path]);
 
 	registerRoute(path.toString(), component);
 
@@ -24,12 +19,17 @@
 	let thisParams = {};
 	let thisWildcards = [];
 
+	let lhome = home || '';
+	lhome = !lhome.startsWith('/') ? '/' + lhome : lhome;
+	lhome = !lhome.endsWith('/') ? lhome + '/' : lhome;
+
 	$: {
 		if ($matched == false && index === true) {
 			thisActive = true;
 			matched.set(true);
 		} else if ($matched == false && !text) {
-			const active = getRegexp(path, exact);
+			console.log(lhome + (path.startsWith('/') ? path.slice(1) : path));
+			const active = getRegexp(lhome + (path.startsWith('/') ? path.slice(1) : path), exact);
 
 			if (routeMatch(location.pathname, active.regex)) {
 				// current route is active
@@ -59,12 +59,7 @@
 </script>
 
 {#if thisActive && component}
-	<svelte:component
-		this={component}
-		params={thisParams}
-		wildcards={thisWildcards}
-		{...$$restProps}
-	/>
+	<svelte:component this={component} params={thisParams} wildcards={thisWildcards} {...$$restProps} />
 {:else if thisActive}
 	<slot params={thisParams} wildcards={thisWildcards} />
 {/if}
